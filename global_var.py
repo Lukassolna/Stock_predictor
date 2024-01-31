45d52066cb3188d77a24d70f8e659e0a27db2b75
lukasja <lukasja@kth.se>
2024-01-31 04:39:39 +0100

    added a larger set of stocks for testing purposes

--- global_var.py
+++ global_var.py
@@ -4,3 +4,18 @@ omx= [
     "HEXA-B.ST", "HM-B.ST", "INVE-B.ST", "KINV-B.ST", "NDA-SE.ST","NIBE-B.ST", "SAND.ST", 
     "SCA-B.ST", "SEB-A.ST", "SINCH.ST", "SBB-B.ST", "SHB-B.ST", "SKF-B.ST",
     "SWED-A.ST", "TEL2-B.ST", "TELIA.ST", "VOLV-B.ST"]
+
+big_omx = [
+    "ABB.ST", "ALFA.ST", "ALIV-SDB.ST", "ASSA-B.ST", "ATCO-A.ST", "ATCO-B.ST", 
+    "AZN.ST", "BOL.ST", "ERIC-B.ST", "ESSITY-B.ST", "EVO.ST", "ELUX-B.ST", "GETI-B.ST", 
+    "HEXA-B.ST", "HM-B.ST", "INVE-B.ST", "KINV-B.ST", "NDA-SE.ST", "NIBE-B.ST", "SAND.ST", 
+    "SCA-B.ST", "SEB-A.ST", "SINCH.ST", "SBB-B.ST", "SHB-B.ST", "SKF-B.ST",
+    "SWED-A.ST", "TEL2-B.ST", "TELIA.ST", "VOLV-B.ST", 
+    "TSLA", "META", "MSFT", "GOOGL", "AAPL", "AMZN", "NFLX", "NVDA", "INTC", "AMD",
+    "CSCO", "ORCL", "IBM", "QCOM", "TXN", "V", "MA", "PYPL", "ADBE", "CRM",
+    "UBER", "LYFT", "SPOT", "T", "VZ", "KO", "PEP", "PG", "JNJ", "MRK",
+    "PFE", "GILD", "ABBV", "AMGN", "BMY", "CVS", "WMT", "MCD", "DIS", "BA",
+    "CAT", "GE", "MMM", "HON", "UNP", "UPS", "FDX", "RTX", "NKE", "SBUX",
+    "XOM", "CVX", "BP", "SHEL", "HSBC", "BAC", "JPM", "GS",
+    "MS", "C", "WFC", "BLK", "AXP", "BRK-B", "LMT", "NOC", "GD"
+]
