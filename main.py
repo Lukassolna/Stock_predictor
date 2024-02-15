d7caf61fe9468343cfab893109c0a0136b108252
lukasja <lukasja@kth.se>
2024-02-15 02:45:58 +0100

    adjustment to preprocessing

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def change_percentage(df):
     return df
 
 def change_column(df):
-    # Create 'next_day_change' column by shifting 'Change' up by one
+    # Create 'next_day_change' column
     df['next_day_change'] = df['Change'].shift(-1)
     # Fill the last row's 'next_day_change' with 0
     df['next_day_change']=df['next_day_change'].fillna(0)
@@ -42,7 +42,6 @@ def add_percentage_diff(df, price_column, sma_column):
 def add_10_day_change(df, change_column):
     # Ensure the 'Change' column is numeric
     df[change_column] = df[change_column].astype(float)
-
     # Calculate the 10-day sum of changes
     df['temp']=df[change_column] /100 +1
     df['10_change'] = (df['temp'].rolling(window=10, min_periods=1).apply(np.prod, raw=True) - 1) * 100
